57735bee0c06a4d348a47822f53af66c73688a11
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-02 10:13:35 +0100

    Fetch and list users

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ContentHeader } from "../../App.styles";
+import axios from "axios";
 
 function Users() {
+  const API_USERS_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_USERS_ENDPOINT).then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
   return (
     <div className="users">
       <ContentHeader>
         <h3>List of users</h3>
       </ContentHeader>
       <ul>
-        <li>user 1</li>
-        <li>user 2</li>
-        <li>user 3</li>
+        {users &&
+          users.map((user) => {
+            return (
+              <li key={user.id}>
+                {user.name} - {user.username} - {user.email}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
