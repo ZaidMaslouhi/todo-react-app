4d359571de8926dfd74d90757009d28889204a50
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-02 10:46:49 +0100

    Fetch and list Todos

--- src/Components/Todos/Todos.jsx
+++ src/Components/Todos/Todos.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ContentHeader } from "../../App.styles";
+import { useNavigate, useParams } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import axios from "axios";
 
 function Todos() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const API_USERS_ENDPOINT = `https://jsonplaceholder.typicode.com/todos?userId=${id}`;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_USERS_ENDPOINT).then((response) => {
+      setTodos(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <ContentHeader>
+        <IoIosArrowBack data-testid="back-icon" onClick={() => navigate("/")} />
         <h3>Todos</h3>
       </ContentHeader>
       <ul>
-        <li>todo 1</li>
-        <li>todo 2</li>
-        <li>todo 3</li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            title: {todo.title} - completed: {todo.completed}
+          </li>
+        ))}
       </ul>
     </div>
   );
