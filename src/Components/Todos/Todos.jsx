af22c3ba58792408c9e1ba2034b04ce62b04eb7d
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-02 11:17:33 +0100

    Fetch todos using useFetch custom hook

--- src/Components/Todos/Todos.jsx
+++ src/Components/Todos/Todos.jsx
@@ -1,33 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import Todo from "./Todo";
 import { ContentHeader, List } from "../../App.styles";
 import { useNavigate, useParams } from "react-router-dom";
-import { IoIosArrowBack } from "react-icons/io";
-import axios from "axios";
-import Todo from "./Todo";
+import useFetch from "../../hooks/useFetch";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Todos() {
   const { id } = useParams();
+  const { data: todos, loading, error } = useFetch(`/todos?userId=${id}`);
   const navigate = useNavigate();
-  const API_USERS_ENDPOINT = `https://jsonplaceholder.typicode.com/todos?userId=${id}`;
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios.get(API_USERS_ENDPOINT).then((response) => {
-      setTodos(response.data);
-    });
-  }, []);
 
   return (
-    <div>
+    <div data-testid="todos">
       <ContentHeader>
         <IoIosArrowBack data-testid="back-icon" onClick={() => navigate("/")} />
         <h3>Todos</h3>
       </ContentHeader>
-      <List>
-        {todos.map((todo) => (
-          <Todo key={todo.id} title={todo.title} completed={todo.completed} />
-        ))}
-      </List>
+      {loading && (
+        <div>
+          <ClipLoader />
+        </div>
+      )}
+
+      {error ? (
+        <div>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <List>
+          {todos.map((todo) => (
+            <Todo key={todo.id} title={todo.title} completed={todo.completed} />
+          ))}
+        </List>
+      )}
     </div>
   );
 }
