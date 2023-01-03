22d5da0f99b3885ced0a9ba2194dae8593772465
ZaidMaslouhi <zaid.maslouhi@gmail.com>
2023-01-03 20:39:38 +0100

    Improve styles & tests in Todo component -Add style to the todo progress -Test the new style

--- src/Components/Todos/styles/todo.styles.js
+++ src/Components/Todos/styles/todo.styles.js
@@ -4,12 +4,18 @@ import { Item } from "../../../App.styles";
 export const TodoCard = styled(Item)`
   border-left: 0.5rem solid
     ${(props) => (props.isCompleted ? "#15ce729a" : "#ed4e329a")};
-  & h5,
-  & p {
+  & h5 {
     margin: 0px;
     text-transform: capitalize;
   }
   & p {
-    font-size: 0.8rem;
+    display: inline;
+    padding: 0.2rem;
+    font-size: 0.7rem;
+    font-weight: 600;
+    background-color: ${(props) =>
+      props.isCompleted ? "#15ce729a" : "#ed4e329a"};
+    color: #fff;
+    border-radius: 0.25rem;
   }
 `;
